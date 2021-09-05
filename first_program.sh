b6ffc9fd9bfdc722634581ab620e61774f47566d
Azim W <waliyani09@gmail.com>
2021-09-04 20:01:59 -0400

    Hello world program v1.1

--- first_program.sh
+++ first_program.sh
@@ -3,3 +3,7 @@
 STRING = "Hello World"
 # This is how you display output
 echo $STRING
+#1 Let us talk about She-bang!
+# It is usually denoted by "#!"
+# She-bang is usually used to denote the path where the shell script is installed.
+# It is an indicator to show where to interpret the given file.
