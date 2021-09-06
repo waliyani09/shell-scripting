894637d67d92ca1288054732c3cf0c5ba118befc
Azim W <waliyani09@gmail.com>
2021-09-06 17:31:43 -0400

    Shell scripting 9/6 v1.0

---
+++ roboshop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo "Installing MongoDB"
+echo "Installing Catalogue"
+echo "Installing Redis"
+echo "Installing User"
+echo "Installing Cart"
+echo "Installing MySQL"
+echo "Installing Shipping"
+echo "Installing RabbitMQ"
+echo "Installing Payment"
+echo "Installing Frontend"
