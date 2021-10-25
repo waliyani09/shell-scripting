792b1d1a97041ff005dfbd2fcb7d69ac494c5cd8
Azim W <waliyani09@gmail.com>
2021-10-25 18:26:19 -0400

    Pushing sed command in the MongoDB.sh file

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -6,6 +6,7 @@ gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 yum install -y mongodb-org 
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl restart mongod
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
