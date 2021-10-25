1ed2d2f81d6884b0990403a4937260b8389da19a
Azim W <waliyani09@gmail.com>
2021-10-25 18:49:53 -0400

    Pushing log action to reudce clutter in the MongoDB.sh file

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -5,13 +5,13 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-yum install -y mongodb-org 
+yum install -y mongodb-org >/tmp/log
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl restart mongod
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
-unzip mongodb.zip
+unzip -o mongodb.zip >/tmp/log
 cd mongodb-main
-mongo < catalogue.js
-mongo < users.js 
+mongo < catalogue.js >/tmp/log
+mongo < users.js >/tmp/log
