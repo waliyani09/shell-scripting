bcc939d57f338929d0d2f0612e6e2b1ae94fa78f
azimwaliyani <waliyani09@gmail.com>
2022-12-26 18:38:50 -0500

    mongo v1.3

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,7 @@
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 yum install -y mongodb-org
 
-sed -i "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf
 systemctl enable mongod
 systemctl restart mongod
 
